7c1beba49e15ca2a0c57ade9444ab2214409efb2
alexis-delebecque <alexis.delebecque@epitech.eu>
2020-08-26 18:13:16 +0200

    Updates

---
+++ precache-manifest.a1046a5a551e7e8fe6bcd1e69bbadd58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c7b03cdd99d6922d4b53059261bc9b1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4f01a9214be31b7188fb",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7a17c1255d3b1a4f042d",
+    "url": "/portfolio/static/js/2.e53e8749.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.e53e8749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f01a9214be31b7188fb",
+    "url": "/portfolio/static/js/main.b74c2298.chunk.js"
+  },
+  {
+    "revision": "c13983abd7561e7e1213",
+    "url": "/portfolio/static/js/runtime-main.ab40c038.js"
+  }
+]);
